a684ac5e6b8aab565aa61437bd3e4b8bed67948e
Diana <aries_di@hotmail.com>
2019-05-13 20:25:03 -0500

    Agrega ruteo de home a menu y hace la vista del menú

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+import { APP_ROUTING } from './app.routes';
+
+//Service
+
+//Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
